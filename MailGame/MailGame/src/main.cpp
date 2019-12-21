69ff972be15ab923898a1d5444c0f6ff7e856a6b
Josef Waller <josef@siriusapplications.com>
2019-12-21 13:33:29 -0800

    Set up SFML

--- MailGame/MailGame/src/main.cpp
+++ MailGame/MailGame/src/main.cpp
@@ -1,4 +1,16 @@
+#include <SFML/Graphics.hpp>
 
 int main() {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Test");
+	while (window.isOpen()) {
+		sf::Event e;
+		while(window.pollEvent(e)) {
+			if (e.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear();
+		window.display();
+	}
 	return 0;
 }
