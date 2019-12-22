55b39f88efb194036be0c2194450f2ea224e42eb
Josef Waller <josef@siriusapplications.com>
2019-12-22 14:13:02 -0800

    Draw an empty map of tiles

--- MailGame/MailGame/src/GameMap/GameMap.cpp
+++ MailGame/MailGame/src/GameMap/GameMap.cpp
@@ -14,7 +14,20 @@ GameMap::GameMap(Game* g) {
 
 void GameMap::render(sf::RenderWindow* window) {
 	sf::Sprite s = ResourceLoader::get()->getSprite("tiles/tiles", "empty");
-	window->draw(s);
+	for (int x = 0; x < 20; x++) {
+		for (int y = 0; y < 20; y++) {
+			// Scale of image
+			float scale = 0.3f;
+			// How much to move the file over and down for each x, y coordinate increase
+			// Basically the distance from the center of the tile to the side or bottom,
+			// but not quite since the tile image has extra stuff at the bottom
+			const float X_OFFSET = 64.0f;
+			const float Y_OFFSET = 32.0f;
+			s.setScale(scale, scale);
+			s.setPosition(sf::Vector2f(scale * X_OFFSET * (x - y), scale * Y_OFFSET * (x + y)));
+			window->draw(s);
+		}
+	}
 }
 
 Tile GameMap::getTileAt(size_t x, size_t y) {
