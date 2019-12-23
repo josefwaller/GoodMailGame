651e4eef1f1d40c0ffa7ddb6271278342a8e64a7
Josef Waller <josef@siriusapplications.com>
2019-12-23 12:32:25 -0800

    Add Line helper class

---
+++ MailGame/MailGame/src/GameMap/Line/Line.cpp
@@ -0,0 +1,37 @@
+#include "Line.h"
+#include <stdlib.h>
+
+Line::Line(sf::Vector2i start, unsigned len, bool isVertical) : start(start), len(len), isVertical(isVertical) {
+
+}
+
+bool Line::isNextTo(Line other) {
+	// First check they are both vertical/horizontal
+	if (this->isVertical != other.isVertical) {
+		return false;
+	}
+	if (this->isVertical) {
+		// If they are one away on the x axis and the lines overlap in the y axis, they are directly beside each other
+		return Line::isDirectlyBeside(this->start.x, other.start.x) && Line::rangesOverlap(this->start.y, this->len, other.start.y, other.len);
+	}
+	else {
+		// Similarly, if one away on the y axis and overlapping on the x axis, they are beside each other
+		return Line::isDirectlyBeside(this->start.y, other.start.y) && Line::rangesOverlap(this->start.x, this->len, other.start.x, other.len);
+	}
+}
+
+bool Line::numInRange(unsigned num, unsigned start, unsigned end) {
+	return num >= start && num <= start + end;
+}
+
+bool Line::rangesOverlap(unsigned startOne, unsigned endOne, unsigned startTwo, unsigned endTwo) {
+	// Check if either startOne or endOne is inbetween startTwo and endTwo
+	if (numInRange(startOne, startTwo, endTwo) || numInRange(endOne, startTwo, endTwo)) {
+		return true;
+	}
+	// Check if startTwo or endTwo is inbetween startOne and endOne
+	return numInRange(startTwo, startOne, endOne) || numInRange(endTwo, startOne, endOne);
+}
+bool Line::isDirectlyBeside(unsigned one, unsigned two) {
+	return abs((int)(one - two));
+}
---
+++ MailGame/MailGame/src/GameMap/Line/Line.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "SFML/System/Vector2.hpp"
+
+/*
+ * A simple line with some utility functions, used to represent roads
+ * during city generation
+ */
+class Line {
+public:
+	Line(sf::Vector2i pos, unsigned len, bool isVertical);
+	// Check if the line is directly next to another line
+	// i.e. they are parallel and touch
+	// This creates weird roads we don't want
+	bool isNextTo(Line other);
+
+private:
+	// The start and length of the line
+	sf::Vector2i start;
+	unsigned len;
+	// Whether the line is vertical or horizontal
+	bool isVertical;
+	// Check if a number is withing a range
+	static bool numInRange(unsigned num, unsigned start, unsigned end);
+	// Check if two ranges overlap
+	static bool rangesOverlap(unsigned startOne, unsigned lenOne, unsigned startTwo, unsigned lenTwo);
+	// Just check if abs(one - two) <= 1;
+	// Mainly used for self-explanatory code
+	static bool isDirectlyBeside(unsigned one, unsigned two);
+};
