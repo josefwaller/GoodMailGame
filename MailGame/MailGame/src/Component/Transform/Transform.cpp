e11c73b1682e7cd914aad98122f439d13e8e4a53
Josef Waller <josef@siriusapplications.com>
2020-01-01 11:47:46 -0800

    Add IsoRotation to transform

--- MailGame/MailGame/src/Component/Transform/Transform.cpp
+++ MailGame/MailGame/src/Component/Transform/Transform.cpp
@@ -1,7 +1,8 @@
 #include "Transform.h"
 
-Transform::Transform(sf::Vector2f pos) {
+Transform::Transform(sf::Vector2f pos, IsoRotation rot) {
 	this->position = pos;
+	this->rotation = rot;
 }
 sf::Vector2f Transform::getPosition() {
 	return this->position;
@@ -9,4 +10,12 @@ sf::Vector2f Transform::getPosition() {
 
 void Transform::setPosition(sf::Vector2f pos) {
 	this->position = pos;
+}
+
+IsoRotation Transform::getRotation() {
+	return this->rotation;
+}
+
+void Transform::setRotation(IsoRotation rot) {
+	this->rotation = rot;
 }
--- MailGame/MailGame/src/Component/Transform/Transform.h
+++ MailGame/MailGame/src/Component/Transform/Transform.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "Component/Component.h"
+#include "System/IsoRotation/IsoRotation.h"
 #include <SFML/System/Vector2.hpp>
 
 class Transform: public Component {
 public:
-	Transform(sf::Vector2f initPos);
+	Transform(sf::Vector2f initPos, IsoRotation rot = IsoRotation());
 	sf::Vector2f getPosition();
 	void setPosition(sf::Vector2f pos);
 
+	IsoRotation getRotation();
+	void setRotation(IsoRotation rot);
 private:
 	sf::Vector2f position;
+	IsoRotation rotation;
 };
