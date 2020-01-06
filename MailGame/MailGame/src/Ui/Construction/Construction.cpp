9b85c6ded54217f44b4f1d63b3f33b12f79db8aa
Josef Waller <josef@siriusapplications.com>
2020-01-06 10:51:16 -0800

    Round position for building post office

--- MailGame/MailGame/src/Ui/Construction/Construction.cpp
+++ MailGame/MailGame/src/Ui/Construction/Construction.cpp
@@ -1,12 +1,13 @@
 #include "Construction.h"
 #include "Entity/EntityPresets/EntityPresets.h"
 #include "Game/Game.h"
+#include <math.h>
 
 std::map<EntityTag, Construction::Recipe> Construction::recipes = {
 	{ EntityTag::PostOffice, {
 		[](Game* g, sf::Vector2f pos, IsoRotation rot) {
-			sf::Vector2i tilesPos(pos);
-			std::shared_ptr<Entity> e = EntityPresets::postOffice(g, pos, rot);
+			sf::Vector2i tilesPos(round(pos.x), round(pos.y));
+			std::shared_ptr<Entity> e = EntityPresets::postOffice(g, sf::Vector2f(tilesPos), rot);
 			g->getGameMap()->setBuildingForTile(tilesPos.x, tilesPos.y, e);
 			return e;
 		},
