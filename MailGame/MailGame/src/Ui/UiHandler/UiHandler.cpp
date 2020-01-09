6f31a677e0d79053d7951bb069e5222b42d32e23
Josef Waller <josef@siriusapplications.com>
2020-01-09 09:10:17 -0800

    Add UiState

--- MailGame/MailGame/src/Ui/UiHandler/UiHandler.cpp
+++ MailGame/MailGame/src/Ui/UiHandler/UiHandler.cpp
@@ -3,21 +3,22 @@
 #include <imgui-SFML.h>
 #include "Game/Game.h"
 
-UiHandler::UiHandler(Game* g): game(g), isBuilding(false) {}
+UiHandler::UiHandler(Game* g): game(g), currentState(UiState::Default) {}
 
 bool UiHandler::handleEvent(sf::Event e) {
 	if (e.type == sf::Event::MouseButtonPressed) {
 		if (ImGui::GetIO().WantCaptureMouse) {
 			return true;
 		}
-		if (isBuilding) {
+		switch(this->currentState) {
+		case UiState::Building:
 			sf::Vector2f mousePos = this->game->getMousePosition();
 			if (this->recipe.positionIsValid(this->game, mousePos, this->currentRotation)) {
 				this->game->addEntity(
 					this->recipe.createFunction(this->game, mousePos, this->currentRotation)
 				);
 			}
-			this->isBuilding = false;
+			this->changeState(UiState::Default);
 			return true;
 		}
 	} else {
@@ -37,10 +38,10 @@ void UiHandler::update() {
 	if (ImGui::CollapsingHeader("Build")) {
 		if (ImGui::Button("Post Office")) {
 			this->recipe = Construction::recipes[EntityTag::PostOffice];
-			this->isBuilding = true;
+			this->changeState(UiState::Building);
 		}
 
-		if (this->isBuilding) {
+		if (this->currentState == UiState::Building) {
 			if (ImGui::Button("Rotate Building")) {
 				this->currentRotation++;
 			}
@@ -51,8 +52,12 @@ void UiHandler::update() {
 }
 
 void UiHandler::render(sf::RenderWindow* w) {
-	if (this->isBuilding) {
+	if (this->currentState == UiState::Building) {
 		bool isValid = this->recipe.positionIsValid(this->game, this->game->getMousePosition(), this->currentRotation);
 		w->draw(this->recipe.getRenderSprite(this->game, this->game->getMousePosition(), this->currentRotation, isValid));
 	}
+}
+
+void UiHandler::changeState(UiState state) {
+	this->currentState = state;
 }
--- MailGame/MailGame/src/Ui/UiHandler/UiHandler.h
+++ MailGame/MailGame/src/Ui/UiHandler/UiHandler.h
@@ -7,15 +7,25 @@ class Game;
 
 class UiHandler {
 public:
+	// The states the UI can be in
+	enum class UiState {
+		Default,
+		Building,
+		Selecting
+	};
+
 	UiHandler(Game* g);
 	bool handleEvent(sf::Event e);
 	void update();
 	void render(sf::RenderWindow* w);
-	void onClick();
+
+	// Change the state
+	// Also will deal with leaving the state it is currently in
+	void changeState(UiState state);
 private:
 	Game* game;
-	// Whether the player is building something
-	bool isBuilding;
+	// Current state of the Ui
+	UiState currentState;
 	// Current recipe the player is building, if anything
 	Construction::Recipe recipe;
 	// The rotation of the current building
