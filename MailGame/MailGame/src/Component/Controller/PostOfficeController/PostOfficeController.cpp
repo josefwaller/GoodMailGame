eb70a3cf8e0bc6d47a5acde886237ced395e19d1
Josef Waller <josef@siriusapplications.com>
2020-01-09 11:17:14 -0800

    Add deleting routes
    Use the route ids to prevent imgui from getting confused.

--- MailGame/MailGame/src/Component/Controller/PostOfficeController/PostOfficeController.cpp
+++ MailGame/MailGame/src/Component/Controller/PostOfficeController/PostOfficeController.cpp
@@ -6,18 +6,21 @@
 
 void PostOfficeController::update(float delta) {
 	if (this->getEntity()->transform->isOnScreen()) {
+		ImGui::PushID(this->getEntity()->getId());
 		ImGui::Begin("Post Office");
 		for (auto it = this->routes.begin(); it != this->routes.end(); it++) {
-			// Get teh route index
+			// Get the route index
 			size_t index = (size_t)(it - this->routes.begin());
+			// Get the ID as a string, for labels
+			ImGui::PushID(it->id);
 			// Create a collapsing header
-			std::string routeName = "Route " + std::to_string((int)(it - this->routes.begin()));
+			std::string routeName = "Route " + std::to_string(it->id);
 			if (ImGui::CollapsingHeader(routeName.c_str())) {
 				// Add dropdown for time
 				if (ImGui::BeginCombo("Departure Time", std::to_string(it->departTime).c_str())) {
 					for (size_t i = 0; i < 24; i++) {
 						if (ImGui::Selectable(std::to_string(i).c_str())) {
-							this->setRouteTime(index, i);
+							this->setRouteTime(index, (int)i);
 						}
 						if (it->departTime == i) {
 							ImGui::SetItemDefaultFocus();
@@ -25,13 +28,27 @@ void PostOfficeController::update(float delta) {
 					}
 					ImGui::EndCombo();
 				}
+				if (ImGui::Button("Delete")) {
+					this->deleteRoute(index);
+				}
 			}
+			ImGui::PopID();
 		}
 		if (ImGui::Button("Create Route")) {
 			this->routes.push_back(MailTruckRoute(true, 0));
 		}
 		ImGui::End();
+		ImGui::PopID();
 	}
+	// Delete any routes that are flagged for deletion
+	for (int i = (int)(routes.size() - 1); i >= 0; i--) {
+		// Check if the route should be deleted
+		if (std::find(this->routesToDelete.begin(), this->routesToDelete.end(), i) != this->routesToDelete.end()) {
+			// Delete it
+			this->routes.erase(this->routes.begin() + i);
+		}
+	}
+	this->routesToDelete.clear();
 }
 
 void PostOfficeController::setRouteTime(size_t routeIndex, int time) {
@@ -39,4 +56,7 @@ void PostOfficeController::setRouteTime(size_t routeIndex, int time) {
 }
 void PostOfficeController::setRouteType(size_t routeIndex, bool isDelivering) {
 	this->routes[routeIndex].isDelivering = isDelivering;
+}
+void PostOfficeController::deleteRoute(size_t routeIndex) {
+	routesToDelete.push_back(routeIndex);
 }
--- MailGame/MailGame/src/Component/Controller/PostOfficeController/PostOfficeController.h
+++ MailGame/MailGame/src/Component/Controller/PostOfficeController/PostOfficeController.h
@@ -10,6 +10,10 @@ public:
 	// Update the attributes from the Ui
 	void setRouteTime(size_t routeIndex, int departTime);
 	void setRouteType(size_t routeIndex, bool isDelivering);
+	void deleteRoute(size_t routeIndex);
 private:
+	// The routes that the post office will spawn trucks for
 	std::vector<MailTruckRoute> routes;
+	// The routes that should be deleted
+	std::vector<size_t> routesToDelete;
 };
