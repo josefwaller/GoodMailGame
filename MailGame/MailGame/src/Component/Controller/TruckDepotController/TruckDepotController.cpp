3f68e02281d32b9bd069189114566619c6c21fc0
Josef Waller <josef@siriusapplications.com>
2020-02-03 09:31:58 -0800

    Add creating/destroying CargoTruckRoutes

--- MailGame/MailGame/src/Component/Controller/TruckDepotController/TruckDepotController.cpp
+++ MailGame/MailGame/src/Component/Controller/TruckDepotController/TruckDepotController.cpp
@@ -1,7 +1,47 @@
 #include "TruckDepotController.h"
+#include <imgui.h>
+#include "Entity/Entity.h"
 
 void TruckDepotController::update(float delta) {
+	ImGui::PushID((int)this->getEntity()->getId());
+	ImGui::Begin("Truck Depot");
+
+	// Show gui for each route
+	for (size_t i = 0; i < this->routes.size(); i++) {
+		CargoTruckRoute route = this->routes[i];
+		ImGui::PushID((int)route.id);
+
+		char buf[200];
+		sprintf_s(buf, "Route %zu", route.id);
+		if (ImGui::CollapsingHeader(buf)) {
+			// Delete button
+			if (ImGui::Button("Delete")) {
+				this->deleteRoute(route.id);
+			}
+		}
+
+		ImGui::PopID();
+	}
+	// Add Route button
+	if (ImGui::Button("Create new route")) {
+		this->addRoute(CargoTruckRoute(0));
+	}
+
+	ImGui::End();
+	ImGui::PopID();
 }
 void TruckDepotController::onHourChange(size_t newHour) {
 
+}
+
+void TruckDepotController::addRoute(CargoTruckRoute r) {
+	this->routes.push_back(r);
+}
+void TruckDepotController::deleteRoute(size_t id) {
+	for (size_t i = 0; i < this->routes.size(); i++) {
+		if (this->routes[i].id == id) {
+			this->routes.erase(this->routes.begin() + i);
+			return;
+		}
+	}
 }
--- MailGame/MailGame/src/Component/Controller/TruckDepotController/TruckDepotController.h
+++ MailGame/MailGame/src/Component/Controller/TruckDepotController/TruckDepotController.h
@@ -6,6 +6,11 @@ class TruckDepotController : public Controller {
 public:
 	virtual void update(float delta) override;
 	virtual void onHourChange(size_t newHour) override;
+
+	// Add a new route
+	void addRoute(CargoTruckRoute route);
+	// Delete a route by id
+	void deleteRoute(size_t routeId);
 private:
 	// The routes assigned to the depot
 	std::vector<CargoTruckRoute> routes;
