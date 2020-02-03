57e7b2958215eaceb7042809a4eca108ba3000b4
Josef Waller <josef@siriusapplications.com>
2020-02-03 11:02:24 -0800

    Add postal codes to tiles

--- MailGame/MailGame/src/GameMap/Tile.h
+++ MailGame/MailGame/src/GameMap/Tile.h
@@ -12,10 +12,14 @@ enum class TileType {
 };
 
 struct Tile {
+	// The type of tile
 	TileType type;
+	// The building on the tile
 	std::weak_ptr<Entity> building;
+	// The postal code assigned to the tile
+	unsigned long long postalCode;
 
-	Tile(TileType t = TileType::Empty) {
+	Tile(TileType t = TileType::Empty): postalCode(0) {
 		type = t;
 	}
 };
