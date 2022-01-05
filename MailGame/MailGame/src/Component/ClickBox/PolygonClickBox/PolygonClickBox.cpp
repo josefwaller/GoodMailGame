234e5329de40c73a4df85736029e1ba9623d367f
Josef Waller <josef@siriusapplications.com>
2022-01-05 12:29:43 -0800

    Add checking whether the mouse is inside the polygon or not

--- MailGame/MailGame/src/Component/ClickBox/PolygonClickBox/PolygonClickBox.cpp
+++ MailGame/MailGame/src/Component/ClickBox/PolygonClickBox/PolygonClickBox.cpp
@@ -6,16 +6,51 @@
 PolygonClickBox::PolygonClickBox(Polygon p) : poly(p) {}
 
 bool PolygonClickBox::checkIfClicked(sf::Vector2f mouseCoords) {
-	return false;
+	std::vector<sf::Vector2f> screenPoints;
+	for (auto p : this->poly.getPoints()) {
+		screenPoints.push_back(this->getEntity()->getGame()->worldToScreenPos(p));
+	}
+	// Used 2D ray casting
+	size_t numHits = 0;
+	for (size_t i = 0; i < screenPoints.size(); i++) {
+		sf::Vector2f pOne = screenPoints.at(i);
+		sf::Vector2f pTwo = screenPoints.at((i + 1) % screenPoints.size());
+		// One must be above and one must be below
+		if ((pOne.y > mouseCoords.y && pTwo.y <= mouseCoords.y) || (pOne.y <= mouseCoords.y && pTwo.y > mouseCoords.y)) {
+			// Edge case: The line is vertical
+			// quickly check for that
+			if (pOne.x == pTwo.x) {
+				if (pOne.x > mouseCoords.x) {
+					numHits++;
+				}
+				continue;
+			}
+			// Check that the point's collision with the line is inbetween thw two points
+			float m = (pOne.y - pTwo.y) / (pOne.x - pTwo.x);
+			float b = pOne.y - m * pOne.x;
+
+			float x = (mouseCoords.y - b) / m;
+
+			float minX = std::fminf(pOne.x, pTwo.x);
+			float maxX = minX + fabsf(pOne.x - pTwo.x);
+			if (x > minX && x < maxX) {
+				if (x > mouseCoords.x) {
+					numHits++;
+				}
+			}
+		}
+	}
+	return numHits % 2 == 1;
 }
 
 void PolygonClickBox::renderClickBox(sf::RenderWindow* window) {
 	auto points = this->poly.getPoints();
 	Game* g = this->getEntity()->getGame();
 	sf::VertexArray arr(sf::PrimitiveType::LinesStrip, points.size() + 1);
+	sf::Color c = this->checkIfClicked(sf::Vector2f(g->getWindowMousePosition())) ? sf::Color::Green : sf::Color::Red;
 	for (auto it = points.begin(); it != points.end(); it++) {
-		arr[it - points.begin()] = sf::Vertex(g->worldToScreenPos(*it), sf::Color::Red);
+		arr[it - points.begin()] = sf::Vertex(g->worldToScreenPos(*it), c);
 	}
-	arr[points.size()] = sf::Vertex(g->worldToScreenPos(points.front()), sf::Color::Red);
+	arr[points.size()] = sf::Vertex(g->worldToScreenPos(points.front()), c);
 	window->draw(arr);
 }
